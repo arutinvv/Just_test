19f07616a11a557febe1760a742a2be4878ce93b
unknown <Arutin@DESKTOP-JQMP6GL.Valkenpoort.local>
2018-07-21 09:58:05 +0300

    first commit

---
+++ Start/src/test/java/Steps/LoginStep.java
@@ -0,0 +1,35 @@
+package Steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class LoginStep {
+
+        @Then("^I should see the userforms page")
+    public void IShouldSeeTheUserformsPage () {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+
+    }
+
+    @Given("^I navigate to the login page")
+    public void iNavigateToTheLoginPage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter the username as admin and password as admin")
+    public void iEnterTheUsernameAsAdminAndPasswordAsAdmin() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click login button")
+    public void iClickLoginButton() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+
+    }
+}
